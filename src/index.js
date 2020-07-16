b3c2804737b6ac118805d563834c44f8dbcbad00
warmoa <moawarman@gmail.com>
2020-07-16 23:50:46 +0700

    starter ok

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import './styles.css';
 const Index = () => {
   return <div className="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
   <div className="sm:flex sm:items-center px-6 py-4">
-    <img className="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="https://avatars2.githubusercontent.com/u/4323180?s=400&u=4962a4441fae9fba5f0f86456c6c506a21ffca4f&v=4" alt=""/>
+    <img className="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="https://avatars0.githubusercontent.com/u/47688843?s=460&u=7256b9efffe3fa26205b0b44ecd88364d96cfb56&v=4" alt=""/>
     <div className="text-center sm:text-left sm:flex-grow">
       <div className="mb-4">
-        <p className="text-xl leading-tight">Adam Wathan</p>
+        <p className="text-xl leading-tight">Aditya Warman</p>
         <p className="text-sm leading-tight text-grey-dark">Developer at NothingWorks Inc.</p>
       </div>
       <div>
